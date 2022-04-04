6e6f166d4e202b2d1c5948925d36bd2d993ab671
DragonCoder <rayfradella@yahoo.com>
2022-04-03 22:50:04 -0400

    Alpha v0.0.3

--- src/comp/tax-info.svelte
+++ src/comp/tax-info.svelte
@@ -1,31 +1,105 @@
 <script>
     import TaxSummary from "./tax-summary.svelte"
 
+    // let variations = ["Original","1","2"];
+    const toCurrency = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2
+    })
+
     let tax_info = {
         grossIncome: 0.00,
         filingStatusDeduction : 0.00,
         sect125: 0.00,
         TSA: 0.00,
         WithHolding_tax: 0.00,
-        FICA: 0.00,
         Misc: 0.00,
         NonTSA: 0.00,
-        NetTakeHome: 0.00
+        NetTakeHome: 0.00,
+        over65dedution: 0.00
+    }
+
+    const GetAGI = () =>{
+        return toCurrency.format(tax_info.grossIncome -
+            tax_info.TSA -
+            tax_info.sect125)
+    }
+
+    const CalculateAGI = () => {
+        document.querySelector(".AGI").textContent = GetAGI()
     }
 </script>
-<div class="tax-info" style="display: flex;">
 
+<!-- <div>
+    <p>Select the variation you want to edit:</p>
+    {#each variations as variation}
+        <input type="radio" value={variation} name="variations"> {variation}
+    {/each}
+</div>
+<br>-->
+
+<div class="tax-info" style="display: flex;">
     <div class="border-2">
-        <h2>Tax Information</h2>
+        <h1>
+            <u>
+                Tax Information
+            </u>
+        </h1>
+
         <div class = "border-2">
             <label for="GrossInc">
                 Gross Income:
             </label>
-            <input type="number" bind:value="{tax_info.grossIncome}">
+
+            <input
+                type="number"
+                bind:value="{tax_info.grossIncome}">
+            <h2>
+                Pre-Tax Contributions
+            </h2>
+
+            <div>
+                <label for="Sect125">
+                    Sect 125:
+                </label>
+
+                <input
+                    type="number"
+                    bind:value="{tax_info.sect125}"/>
+
+            </div>
+        <div>
+            <label for="TSA">
+                TSA:
+            </label>
+
+            <input type="number"  bind:value="{tax_info.TSA}"/>
+
         </div>
 
-        <h4>Filing Status</h4>
-        <select name="filing-status" id="filing-status">
+        <div>
+            <br>
+            <button
+                on:click={CalculateAGI}
+                class="calculate-agi">
+                    Calculate AGI
+            </button>
+        </div>
+            <p>Current AGI:
+                <b>
+                    <span class="AGI">
+
+                    </span>
+                </b>
+            </p>
+        </div>
+
+        <h2>
+            Filing Status
+        </h2>
+
+        <select name="filing-status" id="filing-status" >
             <option value="single">Single</option>
             <option value="married">Married</option>
             <option value="Married_s">Married filing separatly</option>
@@ -34,57 +108,57 @@
 
         <div>
             <label for="Sect125">
-                Enter value from table on the right:
+                Search Tax Table for AGI:
             </label>
-            <input type="number"  bind:value="{tax_info.filingStatusDeduction}"/>
-        </div>
 
-        <h4>Pre-Tax Contributions</h4>
-        <div>
-            <label for="Sect125">
-                Sect 125:
-            </label>
-            <input type="number"  bind:value="{tax_info.sect125}"/>
+            <input
+                type="number"
+                bind:value="{tax_info.filingStatusDeduction}"/>
         </div>
 
         <div>
-            <label for="TSA">
-                TSA:
-            </label>
-            <input type="number"  bind:value="{tax_info.TSA}"/>
+            65+ years old
+            <input
+                class="is65orover"
+                type="checkbox"
+                bind:checked="{tax_info.over65dedution}">
         </div>
 
-        <h4>Witholding Taxes</h4>
+        <h2>
+            Witholding Taxes
+        </h2>
 
         <div>
             <label for="witholding">
                 W/H Tax:
             </label>
-            <input type="number"  bind:value="{tax_info.WithHolding_tax}"/>
-        </div>
 
-        <div>
-            <label for="FICA">
-                FICA:
-            </label>
-            <input type="number"  bind:value="{tax_info.FICA}"/>
+            <input
+                type="number"
+                bind:value="{tax_info.WithHolding_tax}"/>
         </div>
 
         <div>
             <label for="Misc">
                 MISC:
             </label>
-            <input type="number"  bind:value="{tax_info.Misc}"/>
+            <input
+                type="number"
+                bind:value="{tax_info.Misc}"/>
         </div>
 
         <div>
             <label for="NonTSA">
                 Non TSA:
             </label>
-            <input type="number"  bind:value="{tax_info.NonTSA}"/>
+            <input
+                type="number"
+                bind:value="{tax_info.NonTSA}"/>
         </div>
     </div>
+</div>
 
+<div>
     <TaxSummary {tax_info}/>
 </div>
 
@@ -93,5 +167,4 @@
         border: 2px solid black;
         padding: 5px;
     }
-
 </style>
